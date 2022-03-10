0d5ec674f553f4616c225f83d070c85a78d8b84e
Caitlin Swickard <caitlin6029@gmail.com>
2022-03-10 16:03:16 -0700

    created JS for buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+// loop through each sound
+sounds.forEach(sound => {
+  // create a button, add class list of btn
+  const btn = document.createElement('button')
+  btn.classlist.add('btn')
+
+  // set inner text to the sound
+  btn.innerText = sound
+
+  // append each btn
+  document.getElementById('buttons').appendChild(btn)
+})
