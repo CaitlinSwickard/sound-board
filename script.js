ea73b2525964064e2e546499599778a15584710e
Caitlin Swickard <caitlin6029@gmail.com>
2022-03-10 16:24:03 -0700

    finished JS for audio playing and stopping

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ sounds.forEach(sound => {
   // set inner text to the sound
   btn.innerText = sound
 
+  // add event listener
+  btn.addEventListener('click', () => {
+    stopSongs()
+    // .play method attempts to begin playback of the media. 
+    // It returns a Promise which is resolved when playback has been successfully started.
+    document.getElementById(sound).play();
+  })
+
   // append each btn
   document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    // this stops the audio from playing when another sound is clicked
+    song.pause()
+    song.currentTime = 0;
+  })
+}
